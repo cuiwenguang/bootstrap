d87b38c3b45e00e66bb53a79f54d66f6e953b5ff
cuiwenguang <7211334@qq.com>
2019-08-15 18:53:20 +0800

    userservice

---
+++ src/main/java/com/cwg/bootstrap/system/service/IUserService.java
@@ -0,0 +1,14 @@
+package com.cwg.bootstrap.system.service;
+
+import java.util.List;
+
+import com.cwg.bootstrap.system.model.User;
+
+public interface IUserService {
+	String login(String account, String password);
+	boolean create(User user);
+	boolean update(User user);
+	int assignRole(Integer userId, List<Integer> roleIds);
+	boolean changePassword(String account, String oldPwd, String newPwd);
+	boolean changeState(Integer userId, String state);
+}
