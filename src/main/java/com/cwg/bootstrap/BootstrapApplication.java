e263310eff34b27895acd29700771d802a6042ab
cuiwenguang <7211334@qq.com>
2019-08-15 18:14:25 +0800

    aa

--- src/main/java/com/cwg/bootstrap/BootstrapApplication.java
+++ src/main/java/com/cwg/bootstrap/BootstrapApplication.java
@@ -11,5 +11,5 @@ public class BootstrapApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BootstrapApplication.class, args);
 	}
-
 }
+
--- src/main/java/com/cwg/bootstrap/system/controller/AuthController.java
+++ src/main/java/com/cwg/bootstrap/system/controller/AuthController.java
@@ -1,5 +1,7 @@
 package com.cwg.bootstrap.system.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +11,13 @@ import com.cwg.bootstrap.system.model.User;
 @RestController
 @RequestMapping("/")
 public class AuthController {
-	@RequestMapping("/login")
+	
+	@PostMapping("/login")
 	public String login(@RequestBody User user) {
 		return "";
+	}
+	
+	@GetMapping("/logout")
+	public void logout() {
 	}
 }
